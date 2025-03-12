bd2aa340052b14c3149b36c9a665bcb0f810c0c3
GUILHERME MOREIRA DE SOUZA <25133025@cfp127aluno.local>
2025-03-12 11:14:53 -0300

    classe quadrado incompleta. fazer a classe quadrado, e as outras classes

---
+++ src/br/com/engSenai/FormasGeometricas.java
@@ -0,0 +1,14 @@
+package br.com.engSenai;
+
+import br.com.engsenai.model.Quadrado;
+
+public class FormasGeometricas {
+	
+public static void main(String[] args) {
+		
+		Quadrado quadrado = new Quadrado();
+		quadrado.setLado(2);
+		
+		quadrado.mostrarDados();
+
+}
